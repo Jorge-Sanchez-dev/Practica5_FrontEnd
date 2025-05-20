b24ecac55c8e9d7d7d7151cc6c0d802ab183e094
Jorge-Sanchez-dev <jorgesl099@gmail.com>
2025-05-20 13:13:48 +0200

    s

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -7,7 +7,7 @@ import * as $_app from "./routes/_app.tsx";
 import * as $api_joke from "./routes/api/joke.ts";
 import * as $greet_name_ from "./routes/greet/[name].tsx";
 import * as $index from "./routes/index.tsx";
-import * as $Counter from "./islands/Counter.tsx";
+import * as $Counter from "./islands/CompOne.tsx";
 import type { Manifest } from "$fresh/server.ts";
 
 const manifest = {
